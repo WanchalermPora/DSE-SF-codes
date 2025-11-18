7cbedc0d3c12263b69628f35d050866cde62f28a
Wanchalerm Pora <wanchp@gmail.com>
2025-11-18 18:04:46 +0700

    Refactor nor3 module to use I0, I1, I2 inputs

--- ch3/nor3.v
+++ ch3/nor3.v
@@ -1,12 +1,12 @@
 // Verilog Code 3.6: Synthesizable Combinational 3-input NOR
 module nor3 (
-    input  A,
-    input  B,
-    input  C,
-    output reg Y // <--- 'reg' is required here
+    input  I0,
+    input  I1,
+    input  I2,
+    output reg O_NOR // <--- 'reg' is required here
 );
     // Procedural block for combinational logic
     always @(*) begin
-        Y = ~(A | B | C);
+        O_NOR = ~(I0 | I1 | I2);
     end
 endmodule
