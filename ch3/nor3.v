18f2b26cd10a312e12425e91e3c492d3c1446ad7
WanchalermPora <wanchp@gmail.com>
2025-11-18 17:57:52 +0700

    Update nor3.v

--- ch3/nor3.v
+++ ch3/nor3.v
@@ -1,7 +1,4 @@
 // Verilog Code 3.6: Synthesizable Combinational 3-input NOR
-// The output 'Y' MUST be declared as 'reg' because it is
-// assigned inside a procedural block (always), even though
-// it represents combinational logic.
 module nor3 (
     input  A,
     input  B,
