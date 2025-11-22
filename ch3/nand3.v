80775389b260b3a0e204e97c361119fe7d0d03ed
WanchalermPora <wanchp@gmail.com>
2025-11-22 23:22:08 +0700

    nand3

---
+++ ch3/nand3.v
@@ -0,0 +1,10 @@
+// Verilog Code 3.X: 3-input NAND gate using primitive instantiation
+module nand3 (
+    input  I0,
+    input  I1,
+    input  I2,
+    output O
+);
+    // Instantiate a 3-input NAND gate primitive
+    nand U1_nand (O, I0, I1, I2);
+endmodule
