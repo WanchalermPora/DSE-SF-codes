3178af6693c6ce03e08e965818a57eaca4379d8b
Wanchalerm Pora <wanchp@gmail.com>
2025-11-23 23:44:40 +0700

    Fix NOR logic implementation in inverted_and3.sv

--- ch3/inverted_and3.sv
+++ ch3/inverted_and3.sv
@@ -1,11 +1,11 @@
 // (System)Verilog Code 3.7: 3-input NOR using always_comb
-module nor3_sv (
+module inverted_and3 (
     input  logic I0
     input  logic I1,
     input  logic I2,
     output logic O_NOR  // logic replaces reg and wire 
 );
     always_comb begin  // always_comb replaces always
-        O_NOR = ~(I0 | I1 | I2);
+        O_NOR = ~I0 & ~I1 & ~I2;
     end
 endmodule
