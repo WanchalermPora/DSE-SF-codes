2c05112dab4fb124032d59da06a1c385407f433c
WanchalermPora <wanchp@gmail.com>
2025-11-24 21:23:39 +0700

    test_bench change

--- ch3/tb_nor3.v
+++ ch3/tb_nor3.v
@@ -4,13 +4,13 @@ module tb_nor3;
     logic dut_out;
     logic ref_out;
 
-    // Instantiate DUT (behavioural description of inverted AND)
+    // Instantiate DUT (inverted AND)
     inverted_and3 dut (
         .I0(I0), .I1(I1), .I2(I2),
         .O_NOR(dut_out)
     );
 
-    // Instantiate reference model (structural description)
+    // Instantiate reference model (NOR)
     nor U_ref (ref_out, I0, I1, I2);
 
     initial begin
@@ -19,15 +19,12 @@ module tb_nor3;
             {I0, I1, I2} = vec[2:0];
             #10;
             if (dut_out !== ref_out) begin
-                $display("Mismatch: I0 I1 I2 = %b%b%b  DUT = %b  REF = %b",
-                         I0, I1, I2, dut_out, ref_out);
+                $display("Mismatch: I0 I1 I2 = %b%b%b  DUT = %b  REF = %b", I0, I1, I2, dut_out, ref_out);
             end else begin
-                $display("Match: I0 I1 I2 = %b%b%b  Output = %b",
-                         I0, I1, I2, dut_out);
+                $display("Match: I0 I1 I2 = %b%b%b  Output = %b", I0, I1, I2, dut_out);
             end
         end
         $display("Testbench completed");
         $finish;
     end
-
 endmodule
